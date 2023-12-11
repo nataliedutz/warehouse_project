d6d909862b9e09f6e0ec97c7886479176619c724
nataliedutz <natalie.dutz@dci-student.org>
2023-12-11 20:17:56 +0100

    Applying flake8 to classes and test_class  files

--- cli/classes.py
+++ cli/classes.py
@@ -1,13 +1,6 @@
-from datetime import datetime
-from collections import defaultdict
-from data import personnel, stock
 from loader import Loader
-import colors as colors
-from typing import List
+import colors
 
-#from datetime import datetime
-from loader import Loader
-import colors 
 
 class User:
     def __init__(self, user_name="Anonymous", password=None):
@@ -24,7 +17,8 @@ class User:
         Returns:
             bool: True if authentication is successful, False otherwise.
         """
-        self.is_authenticated = False  # Placeholder for actual authentication logic
+        # Placeholder for actual authentication logic
+        self.is_authenticated = False
         return self.is_authenticated
 
     def is_named(self, name):
@@ -43,7 +37,11 @@ class User:
         """
         Display a greeting message for the user.
         """
-        print(f"{colors.ANSI_PURPLE}{' ' * 30}Hello, {self._name}!\n{' ' * 20}Welcome to our Warehouse Database.\n{' ' * 16}If you don't find what you are looking for,\n{' ' * 14}Please ask one of our staff members to assist you.{colors.ANSI_RESET}")
+        print(
+            f"{colors.ANSI_PURPLE}{' ' * 30}Hello, {self._name}!\n{' ' * 20}"
+            f"Welcome to our Warehouse Database.\n{' ' * 16}If you don't find "
+            f"what you are looking for,\n{' ' * 14}Please ask one of our "
+            f"staff members to assist you.{colors.ANSI_RESET}")
 
     def bye(self, actions):
         """
@@ -52,10 +50,16 @@ class User:
         Args:
             actions (list): List of actions taken during the session.
         """
-        print(f"{colors.ANSI_BLUE}\n{'-' * 75}  Thank you for your visit, {self._name}.  {'-' * 75}{colors.ANSI_RESET}\n")
+        print(
+            f"{colors.ANSI_BLUE}\n{'-' * 75}  Thank you for "
+            f"your visit, {self._name}.  {'-' * 75}{colors.ANSI_RESET}\n"
+            )
 
         if len(actions) == 0:
-            print(f"\n{colors.ANSI_RESET}{' ' * 20}You have not done any action in specific!")
+            print(
+                f"\n{colors.ANSI_RESET}{' ' * 20}"
+                f"You have not done any action in specific."
+                )
         else:
             print(f"{colors.ANSI_RESET}\nSummary of action this session:")
             for id, stmt in enumerate(actions):
@@ -77,18 +81,19 @@ class Employee(User):
         print(f"User {self._name} ordered {amount} {item}")
 
     def greet(self):
-        print(f"Hello, {self._name}!\nIf you experience a problem with the system, \nplease contact technical support.")
+        print(
+            f"Hello, {self._name}!\nIf you experience a problem "
+            f"with the system, \nplease contact technical support."
+            )
 
     def bye(self, actions):
         super().bye(actions)
-        if actions:
-            print("\nSummary of actions this session:")
-            for id, stmt in enumerate(actions):
-                print(" " * 20, id + 1, ".", stmt)
 
 
 class Item:
-    def __init__(self, state=None, category=None, date_of_stock=None, warehouse=None):
+    def __init__(
+            self, state=None, category=None, date_of_stock=None, warehouse=None
+            ):
         self.state = state
         self.category = category
         self.date_of_stock = date_of_stock
@@ -143,19 +148,18 @@ class Warehouse:
 
         for item in self.stock:
             if str(item).lower() == search_item.lower():
-                if isinstance(item, Item):  # Check if item is an instance of the Item class
+                # Check if item is an instance of the Item class
+                if isinstance(item, Item):
                     search_item_list.append((item, item.date_of_stock))
                 else:
-                    # If item is a string representation, create a dummy Item without date
+                    # If item is string repr., create pseudo Item without date
                     search_item_list.append((Item(), None))
 
         return search_item_list
 
-
     def __str__(self):
         """
         Return a string representing the warehouse.
-
         Returns:
             str: The string representation of the warehouse.
         """
@@ -173,14 +177,16 @@ class Warehouse:
         for warehouse in stock:
             for item in warehouse.stock:
                 list_item_category.append(item.category)
-        dict_item_category_count = {i: list_item_category.count(i) for i in list_item_category}
+        dict_item_category_count = {
+            i: list_item_category.count(i) for i in list_item_category
+            }
         dict_id_category = {}
         print()
         for id, (key, value) in enumerate(dict_item_category_count.items()):
             dict_id_category[id + 1] = key
-            print(f"{' ' * 20}{colors.ANSI_PURPLE}{id + 1} {key} ({value}){colors.ANSI_RESET}")
+            print(
+                f"{' ' * 20}{colors.ANSI_PURPLE}{id + 1} "
+                f"{key} ({value}){colors.ANSI_RESET}"
+                )
         print()
         return dict_id_category
-
-
-
--- cli/test_class.py
+++ cli/test_class.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from classes import Warehouse, Item, User, Employee
 
 
-
 class TestClasses(unittest.TestCase):
 
     def setUp(self):
@@ -11,20 +10,32 @@ class TestClasses(unittest.TestCase):
 
     def test_classes_exist(self):
         # Check if Warehouse class exists
-        self.assertTrue(hasattr(Warehouse, '__name__'), "Warehouse class does not exist.")
+        self.assertTrue(
+            hasattr(Warehouse, '__name__'), "Warehouse class does not exist."
+            )
 
         # Check if Item class exists
-        self.assertTrue(hasattr(Item, '__name__'), "Item class does not exist.")
+        self.assertTrue(
+            hasattr(Item, '__name__'), "Item class does not exist."
+            )
 
         # Check if User class exists
-        self.assertTrue(hasattr(User, '__name__'), "User class does not exist.")
+        self.assertTrue(
+            hasattr(User, '__name__'), "User class does not exist."
+            )
 
-        # Check if Employee class exists 
-        self.assertTrue(hasattr(Employee, '__name__'), "Employee class does not exist.")
+        # Check if Employee class exists
+        self.assertTrue(hasattr(
+            Employee, '__name__'), "Employee class does not exist."
+            )
 
     def test_for_inheritance(self):
-        #Check if employee class inherits from User class
-        self.assertTrue(issubclass(Employee, User), "Employee class does not inherit from User.")
+        # Check if employee class inherits from User class
+        self.assertTrue(
+            issubclass(Employee, User),
+            "Employee class does not inherit from User."
+            )
+
 
 class TestUser(unittest.TestCase):
 
@@ -49,6 +60,7 @@ class TestUser(unittest.TestCase):
         self.assertTrue(user.is_named("Alice"))
         self.assertFalse(user.is_named("Bob"))
 
+
 class TestEmployee(unittest.TestCase):
 
     def test_employee_creation(self):
@@ -71,9 +83,10 @@ class TestEmployee(unittest.TestCase):
         actions = []
         # Define the item_name variable representing the item's name
         item_name = "example_item"
-        # Call the 'order' method of the Employee object to place an order for 5 'item_name'
+        # Call the 'order' method of the Employee object
+        # to place an order for 5 'item_name'
         employee.order(item_name, 5)
-         # Append the action performed to the 'actions' list
+        # Append the action performed to the 'actions' list
         actions.append(f"Ordered 5 {item_name}")
         # Assert that the 'actions' list contains the expected action
         self.assertEqual(actions, [f"Ordered 5 {item_name}"])
@@ -92,7 +105,8 @@ class TestWarehouse(unittest.TestCase):
         warehouse_id = 1
         # Create a warehouse with the specified warehouse_id
         warehouse = Warehouse(warehouse_id)
-        # Check if warehouse_id property contains the same value as the argument passed
+        # Check if warehouse_id property contains
+        # the same value as the argument passed
         self.assertEqual(warehouse.warehouse_id, warehouse_id)
 
     def test_warehouse_creation_stock_is_empty_list(self):
@@ -109,7 +123,8 @@ class TestWarehouse(unittest.TestCase):
         item2 = Item(state="used", category="books")
         warehouse.add_item(item1)
         warehouse.add_item(item2)
-        # Check if the occupancy method returns the same as the length of the stock property
+        # Check if the occupancy method returns
+        # the same as the length of the stock property
         self.assertEqual(warehouse.occupancy(), len(warehouse.stock))
 
     def test_add_item_increases_total_items_in_stock(self):
@@ -118,7 +133,8 @@ class TestWarehouse(unittest.TestCase):
         # Add an item to the warehouse's stock
         item = Item(state="new", category="electronics")
         warehouse.add_item(item)
-        # Check if the length of the stock property increases after adding an item
+        # Check if the length of the stock property
+        # increases after adding an item
         self.assertEqual(len(warehouse.stock), 1)
 
     def test_search_method_returns_correct_items(self):
@@ -142,20 +158,26 @@ class TestWarehouse(unittest.TestCase):
         self.assertEqual(search_result[0][0].state, "new")
         self.assertEqual(search_result[0][0].category, "electronics")
 
-        # Search for items with state "used" and category "electronics" (non-existent item)
+        # Search for items with state "used" and category
+        # "electronics" (non-existent item)
         self.assertEqual(len(warehouse.search("used electronics")), 0)
-
 
     class TestItem(unittest.TestCase):
         # Test Case 1
-        item1 = Item(state="New", category="Electronics", date_of_stock=datetime(2023, 1, 15), warehouse="A1")
+        item1 = Item(
+            state="New", category="Electronics",
+            date_of_stock=datetime(2023, 1, 15), warehouse="A1"
+            )
         assert item1.state == "New"
         assert item1.category == "Electronics"
         assert item1.date_of_stock == datetime(2023, 1, 15)
         assert str(item1) == "New Electronics"
 
         # Test Case 2
-        item2 = Item(state="Used", category="Mouse", date_of_stock=datetime(2023, 2, 20), warehouse="B2")
+        item2 = Item(
+            state="Used", category="Mouse",
+            date_of_stock=datetime(2023, 2, 20), warehouse="B2"
+            )
         assert item2.state == "Used"
         assert item2.category == "Mouse"
         assert item2.date_of_stock == datetime(2023, 2, 20)
@@ -167,6 +189,7 @@ class TestWarehouse(unittest.TestCase):
         assert item3.category is None
         assert item3.date_of_stock is None
         assert str(item3) == ""
+
 
 print("All tests passed!")
 
